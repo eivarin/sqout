5eb47a0855c5a4628f830b3c0c789013c7e9c9bd
eivarin <rodrigommpereira309@gmail.com>
2024-04-18 10:13:54 +0100

    dockerrrrr

--- backend/libs/DbApi/DbApi.go
+++ backend/libs/DbApi/DbApi.go
@@ -2,6 +2,9 @@ package DbApi
 
 import (
 	"context"
+	"fmt"
+	"os"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -14,8 +17,15 @@ func NewColFacade(cl *mongo.Client, colName string) ColFacade {
 	return ColFacade{Col: cl.Database("sqout").Collection(colName)}
 }
 
-
 func InitDB(c context.Context) *mongo.Client {
-	client, _ := mongo.Connect(c, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
+	uri := os.Getenv("MONGO_URI")
+	if uri == "" {
+		uri = "mongodb://root:example@localhost:27017"
+	}
+	fmt.Printf("Connecting to mongo using: \"%s\"\n", uri)
+	client, err := mongo.Connect(c, options.Client().ApplyURI(uri))
+	if err != nil {
+		fmt.Printf("Error connecting to mongo: %v\n", err)
+	}
 	return client
 }
